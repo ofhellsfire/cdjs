0d4640ec1ec37de9f7de984eb5c4ce5b38613830
ofhellsfire <ofhellsfire@yandex.ru>
2021-02-27 17:49:20 +0300

    Add license

--- setup.py
+++ setup.py
@@ -6,6 +6,11 @@ setup(
     version="0.1.0",
     rust_extensions=[RustExtension("cdjs.cdjs", binding=Binding.PyO3)],
     packages=["cdjs"],
+    author="ofhellsfire",
+    author_email="ofhellsfire@yandex.ru",
+    description="Custom Datetime JSON Serializer",
+    url="https://github.com/ofhellsfire/cdjs",
+    python_requires='>=3.6',
     # rust extensions are not zip safe, just like C-extensions.
     zip_safe=False,
 )
